8201f82ea5f45eb6ed274af0e793ed099b66a903
hcphi <hcphi@sdc.ud.edu.vn>
2016-09-19 21:43:40 +0700

    Gt/Git/Dao.cs

---
+++ Git/Git/Dao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git
+{
+    class Dao
+    {
+        public void Print()
+        {
+            Console.WriteLine("Develop");
+        }
+    }
+}
